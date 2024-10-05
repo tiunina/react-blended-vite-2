1810f683bfefb9df92a79630d18964ead23a211e
Liza <onishenkolizka99@gmail.com>
2024-10-05 17:16:06 +0300

    task2

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,3 +1,23 @@
-export const TodoListItem = () => {
-  return <h3>TodoListItem</h3>;
+import { RiDeleteBinLine } from 'react-icons/ri';
+import style from './TodoListItem.module.css';
+import { Text } from 'components';
+
+export const TodoListItem = ({ todoItem, counter, deleteTodo }) => {
+  return (
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO #{counter}
+      </Text>
+      <Text>{todoItem.text}</Text>
+      <button
+        className={style.deleteButton}
+        onClick={() => {
+          deleteTodo(todoItem.id);
+        }}
+        type="button"
+      >
+        <RiDeleteBinLine size={24} />
+      </button>
+    </div>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,6 @@
-import { Text, Form } from 'components';
+import { Text, Form, TodoList } from 'components';
 import { useEffect, useState } from 'react';
+
 export const Todos = () => {
   const [todos, setTodos] = useState(
     () => JSON.parse(localStorage.getItem('todosItem')) ?? [],
@@ -14,11 +15,17 @@ export const Todos = () => {
     setTodos([...todos, newToDo]);
   };
 
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <>
       <Form onSubmit={createNewToDo} />
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </>
   );
 };
-
-// <Text textAlign="center">There are no any todos ...</Text>;
